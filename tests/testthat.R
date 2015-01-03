a8a0d8321388e5525482080c1f831dc3f232a0fc
szilard <szilard@noemail>
2015-01-02 23:47:53 -0800

    add tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(shinyvalidinp)
+
+test_check("shinyvalidinp")
---
+++ tests/testthat/test-validinp.R
@@ -0,0 +1,38 @@
+context("validinp functions")
+
+test_that("logical", {
+    expect_equal(validinp_logical(TRUE), TRUE)
+    expect_equal(validinp_logical(FALSE), FALSE)    
+
+    expect_error(validinp_logical(NA))
+    expect_error(validinp_logical(c(TRUE, TRUE)))     
+    expect_error(validinp_logical(logical()))
+    
+    expect_error(validinp_logical(1:2))
+    expect_error(validinp_logical(list(a=1,b=2)))
+    expect_error(validinp_logical(letters))     
+})
+
+test_that("numeric", {
+    expect_equal(validinp_numeric(2), 2)
+    expect_equal(validinp_numeric(2, min=1, max=3), 2)
+    
+    expect_error(valid_numeric(NA))
+    expect_error(valid_numeric(1:2))
+    expect_error(valid_numeric(-1, min=1, max=3))
+    expect_error(valid_numeric(4, min=1, max=3))
+    
+    expect_error(valid_numeric(letters))
+    expect_error(valid_numeric(list(a=1,b=2)))
+    expect_error(valid_numeric(TRUE))
+})
+
+test_that("character", {
+    
+})
+
+test_that("Date", {
+    
+})
+
+
